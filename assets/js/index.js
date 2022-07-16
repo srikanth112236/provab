925363559e08d8df024bef4e503dbd7e73122a19
srikanth112236 <srikanth112236@gmail.com>
2022-07-16 16:23:14 +0530

    files added

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+// Declaring variables for each IDS
+var counterPlaceHolder = document.getElementById("counter-placeholder");
+var btnIncrement = document.getElementById("btn-increment");
+var btnDecrement = document.getElementById("btn-decrement");
+
+var number = 0;
+
+// Function to change color of the number 
+// If number is less than 0 color is red, if more than 0 color is green and if 0, color is white.
+
+function changeColor(number){
+    var color = "";
+    if(number < 0 ){
+        color = "red";
+    }else if (number > 0 ){
+        color = "green";
+    }else{
+        color="white";
+    }
+    return color;
+}
+
+btnIncrement.addEventListener("click", function(){
+    number++;
+    counterPlaceHolder.innerHTML = number;
+    counterPlaceHolder.style.color = changeColor(number);
+});
+
+btnDecrement.addEventListener("click", function(){
+    number--;
+    counterPlaceHolder.innerHTML = number;
+    counterPlaceHolder.style.color = changeColor(number);
+});
